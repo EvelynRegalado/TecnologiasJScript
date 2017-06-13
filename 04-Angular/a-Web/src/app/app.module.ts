bbf3ee887b2e1d5bbf9c48af347ca7b07dea8a9c
USRDEL <evelyn.regalado@epn.edu.ec>
2017-06-13 15:08:49 -0500

    Creación de Componentes

--- 04-Angular/a-Web/src/app/app.module.ts
+++ 04-Angular/a-Web/src/app/app.module.ts
@@ -7,10 +7,15 @@ import { AppComponent } from './app.component';
 import { EstilosComponent } from './estilos/estilos.component';
 import {InicioComponent} from "./Componentes/inicio/inicio.component";
 import { PlanetaStarWarsComponent } from './Componentes/planeta-star-wars/planeta-star-wars.component';
+import { DiameterComponent } from './Componentes/diameter/diameter.component';
+import { ClimateComponent } from './Componentes/climate/climate.component';
+import { GravityComponent } from './Componentes/gravity/gravity.component';
+import { TerrainComponent } from './Componentes/terrain/terrain.component';
+import { OrbitalPeriodComponent } from './Componentes/orbital-period/orbital-period.component';
 @NgModule({
   declarations: [
     AppComponent,
-    EstilosComponent, InicioComponent, PlanetaStarWarsComponent
+    EstilosComponent, InicioComponent, PlanetaStarWarsComponent, DiameterComponent, ClimateComponent, GravityComponent, TerrainComponent, OrbitalPeriodComponent
   ],
   imports: [
     BrowserModule,
